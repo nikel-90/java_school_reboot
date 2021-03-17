3707e9afc58f42832cd4b852ad9f74a56e22e54e
Nikita <NSBazhenov@sberbank.ru>
2021-03-17 22:07:43 +0300

    ready

---
+++ src/main/java/ru/sber/javaschool/homework/hw1/Calculator/Program.java
@@ -0,0 +1,17 @@
+package ru.sber.javaschool.homework.hw1.Calculator;
+
+import java.util.Scanner;
+
+public class Program {
+
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Введите выражение: ");
+            String statement = scanner.nextLine();
+            System.out.println(calculator.evaluate(statement));
+        }
+    }
+}
